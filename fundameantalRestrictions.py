4050b51134b491ae352f4f45e4f1d92e545036f6
Vlad Andrei Butnaru <62260342+vladsuper5555@users.noreply.github.com>
2024-10-30 22:44:36 +0200

    continued work on the backtraking algorithm

---
+++ fundameantalRestrictions.py
@@ -0,0 +1,26 @@
+restrictions = []
+
+def f1 (timeTable):
+    # orice grupa nu are ore simultan
+    length = len(timeTable)
+    for index1 in range(length):
+        for index2 in range(length):
+            if index1 == index2:
+                continue
+
+            if (timeTable[index1][''])
+    pass
+def f2 (timeTable):
+    pass
+def f3 (timeTable):
+    pass
+def f4 (timeTable):
+    pass
+def f5 (timeTable):
+    pass
+
+restrictions.append(f1)
+restrictions.append(f2)
+restrictions.append(f3)
+restrictions.append(f4)
+restrictions.append(f5)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import copy
+import fundamentalRestricitons
 fileNames = ['grupe', 'materii', 'profesori', 'sali', 'timp', 'extraRestrictions']
 loadedData = {}
 # loading the data that we have
@@ -16,14 +17,45 @@ bestTimeTable = None
 bestTimeTableScore = 0
 currentTimetable = {}
 
+hardRestrictions = []
+softRestrictions = []
+
+def addToTimeTable (profesorIndex, timeIndex, grupa, sala, materie):
+    if not profesorIndex in currentTimetable:
+        currentTimetable[profesorIndex] = {}
+
+    currentTimetable[profesorIndex][timeIndex] = (grupa, sala, materie)
+
+def removeFromTimeTAble(profesorIndex, timeIndex, grupa, sala, materie):
+    currentTimetable[profesorIndex].pop(timeIndex)
+
+def isTimeTableValid():
+    #in this function we want to test if the fundamental restrictions are validated
+
 
 def processNextEntry(currentProfesorIndex, currentTimeIndex):
     # here we process the next professor and the next time entry in which we can assign a course
     # logic is if no more time options for current professor
     # we go to the next one only then
-    a = 1
+
+    if currentProfesorIndex == len(loadedData['profesori']):
+        return (-1, -1)
+
+    if currentTimeIndex == 30: 
+        # this means we finished all time slots for current professor 
+        # so we move to the next one 
+        currentProfesorIndex += 1
+        currentTimeIndex = 0
+
+    else:
+        currentTimeIndex += 1
+
+    return (currentProfesorIndex, currentTimeIndex)
 
 def back(profesorIndex, timeIndex):
+    # this means no more professors
+    if profesorIndex == -1 or timeIndex == -1:
+        return
     if isTimeTableValid() and areHardExtraRestricitonsSatisfied():
         timeTableScore = calculateTimeTableScoreBasedOnSoftRestrictions()
         if timeTableScore > bestTimeTableScore:
